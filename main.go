4d54e8ed6ff85474e05c59b4020829b7309e5ee3
hokita <hideee.0202@gmail.com>
2024-07-12 22:43:09 +0900

    Add version option.

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+const version = "1.2.0"
+
 func main() {
+	versionFlag := flag.Bool("version", false, "Display the version number")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("skunk version %s\n", version)
+		os.Exit(0)
+	}
+
 	if err := skunk(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
--- skunk.go
+++ skunk.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func skunk() error {
 	fmt.Println(fart())
